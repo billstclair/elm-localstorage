07ea33a8a8d662c34f6cb1aca89a5d4256326d38
Bill St. Clair <billstclair@gmail.com>
2018-03-25 02:35:22 -0400

    Add PortExample.elm. Duh.

---
+++ example/PortExample.elm
@@ -0,0 +1,59 @@
+----------------------------------------------------------------------
+--
+-- PortExample.elm
+-- LocalStorage example for use with real JS localStorage via Elm ports
+-- Copyright (c) 2018 Bill St. Clair <billstclair@gmail.com>
+-- Some rights reserved.
+-- Distributed under the MIT License
+-- See LICENSE.txt
+--
+----------------------------------------------------------------------
+
+
+port module PortExample exposing (..)
+
+import Html
+import Json.Encode as JE
+import LocalStorage
+import LocalStorage.DictPorts as DictPorts
+import LocalStorage.SharedTypes exposing (Key, Operation(..), Ports, Value)
+import SharedUI exposing (Model, Msg(..), getPorts, init, update, view)
+import Task
+
+
+main =
+    Html.programWithFlags
+        { init = \initialModel -> init initialModel ports
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+ports : Ports Msg
+ports =
+    LocalStorage.makeRealPorts "example" getItem setItem clear
+
+
+wrapper : Operation -> Maybe (Ports Msg) -> Key -> Value -> Cmd Msg
+wrapper operation ports key value =
+    UpdatePorts operation ports key value
+        |> Task.succeed
+        |> Task.perform identity
+
+
+port getItem : ( String, Key ) -> Cmd msg
+
+
+port setItem : ( String, Key, Value ) -> Cmd msg
+
+
+port clear : String -> Cmd msg
+
+
+port receiveItem : (( Key, Value ) -> msg) -> Sub msg
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    receiveItem <| \( key, value ) -> UpdatePorts GetItemOperation Nothing key value
