4b15b2c92e28b9a6b36e5bc68e01d8bbdfbadcc1
Rupert Smith <rupert@thesett.com>
2020-06-16 21:38:16 +0100

    Documented.

--- src/LocalStorage.elm
+++ src/LocalStorage.elm
@@ -32,18 +32,26 @@ import Json.Encode as JE
 -- Port signatures.
 
 
-type alias GetItemPort msg =
-    String -> Cmd msg
-
-
-type alias SetItemPort msg =
-    ( String, JE.Value ) -> Cmd msg
-
-
+{-| Port to clear the whole area of local storage underneath a prefix.
+-}
 type alias ClearPort msg =
     String -> Cmd msg
 
 
+{-| Port to fetch an item by its key and prefix.
+-}
+type alias GetItemPort msg =
+    String -> Cmd msg
+
+
+{-| Port to set the value of an item by key and prefix.
+-}
+type alias SetItemPort msg =
+    ( String, JE.Value ) -> Cmd msg
+
+
+{-| Port to list the available local storage keys underneath a prefix.
+-}
 type alias ListKeysPort msg =
     String -> Cmd msg
 
@@ -52,11 +60,23 @@ type alias ListKeysPort msg =
 --
 
 
+{-| The type of the local storage handle, required to perform operations on
+local storage.
+-}
 type LocalStorage msg
     = LocalStorage ( Ports msg, String )
 
 
-make : GetItemPort msg -> SetItemPort msg -> ClearPort msg -> ListKeysPort msg -> String -> LocalStorage msg
+{-| Builds local storage using the supplied port implementations and a prefix to
+namespace all keys stored in that area of local storage.
+-}
+make :
+    GetItemPort msg
+    -> SetItemPort msg
+    -> ClearPort msg
+    -> ListKeysPort msg
+    -> String
+    -> LocalStorage msg
 make getPort setPort clearPort listKeysPort prefix =
     let
         ports =
@@ -73,24 +93,32 @@ make getPort setPort clearPort listKeysPort prefix =
 -- Operations on local storage.
 
 
+{-| Clears the whole area of local storage underneath a prefix.
+-}
 clear : LocalStorage msg -> Cmd msg
 clear (LocalStorage ( ports, prefix )) =
     addPrefix prefix ""
         |> ports.clear
 
 
+{-| Fetches an item by its key and prefix.
+-}
 getItem : LocalStorage msg -> String -> Cmd msg
 getItem (LocalStorage ( ports, prefix )) key =
     addPrefix prefix key
         |> ports.getItem
 
 
+{-| Sets the value of an item by key and prefix.
+-}
 setItem : LocalStorage msg -> String -> JE.Value -> Cmd msg
 setItem (LocalStorage ( ports, prefix )) key value =
     ( addPrefix prefix key, value )
         |> ports.setItem
 
 
+{-| Lists the available local storage keys underneath a prefix.
+-}
 listKeys : LocalStorage msg -> String -> Cmd msg
 listKeys (LocalStorage ( ports, prefix )) userPrefix =
     addPrefix prefix userPrefix
